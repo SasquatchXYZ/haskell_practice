06ee56dca43a3da6eec4773efadd7cfaf3401f0e
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2021-02-27 11:05:42 -0500

    chapter 6 work - recursion

---
+++ Chapter6/recursive.hs
@@ -0,0 +1,16 @@
+--fac :: Int -> Int
+--fac n = product [1..n]
+
+fac :: Int -> Int
+fac 0 = 1
+fac n = n * fac (n-1)
+
+insert :: Ord a => a -> [a] -> [a]
+insert x [] = [x]
+insert x (y:ys) | x <= y = x : y : ys
+                | otherwise = y : insert x ys
+
+isort :: Ord a => [a] -> [a]
+isort [] = []
+isort (x:xs) = insert x (isort xs)
+
